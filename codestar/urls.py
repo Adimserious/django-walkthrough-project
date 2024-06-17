e8955dcd4335f974070ef92e71f548d0afac67e8
Adimserious Amatobi <lilianadimchinobi@gmail.com>
2024-06-17 08:22:34 +0000

    Genric post view and git pish

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog.views import my_blog
+
 from about import views as about_views
 
 urlpatterns = [
-    path('blog/', my_blog, name='blog'),
+    path('', include("blog.urls"), name='blog-urls'),
     path('about/', about_views.about_me, name='about'),
     path('admin/', admin.site.urls),
 ]
